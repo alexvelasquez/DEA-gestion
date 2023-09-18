39d7eae22d56e32c8afe66ef7eb521689ae0ad85
Alex Velasquez <alexvelasquez94@gmail.com>
2023-09-18 00:29:21 -0300

    [FEAT] config font icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,12 +1,16 @@
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+        defaultSet: 'mdi',
+    },
     theme: {
         themes: {
             light: {
