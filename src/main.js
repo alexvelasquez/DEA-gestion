05fddbfb916a95c44f7aae24ef3ac4a888a1fb8d
Alex Velasquez <alexvelasquez94@gmail.com>
2023-09-21 16:42:10 -0300

    [FEAT] instaled dependencies (sweeralert, animatecss)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import { router } from './router'
 import { vuetify } from './plugins/vuetify'
+
 import Axios from './plugins/axios'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 const app = createApp(App)
     .use(router)
     .use(vuetify)
+    .use(VueSweetalert2)
 app.config.globalProperties.$http = Axios
 app.mount('#app')
---
+++ src/mixins/sweetalert.js
@@ -0,0 +1,38 @@
+export default {
+    methods: {
+        alertSuccess(title,subtitle='') {
+            return this.$swal.fire({
+                title: title,
+                text:subtitle,
+                icon: "success",
+                confirmButtonText: 'Aceptar',
+                cancelButtonText: 'Cancelar',
+                allowOutsideClick:false,
+                confirmButtonColor: '#4D8BA0',
+            });
+        },
+        alertQuestion(title,subtitle="¿Confirmar?") {
+            return this.$swal.fire({
+                title: title,
+                text:subtitle,
+                icon: "question",
+                showCancelButton: true,
+                allowOutsideClick:false,
+                confirmButtonText: 'Si, Confirmar',
+                cancelButtonText: 'Cancelar',
+                confirmButtonColor: '#4D8BA0',
+            });
+        },
+        alertError(title,subtitle='') {
+            return this.$swal.fire({
+                title: title,
+                text:subtitle,
+                icon: "error",
+                confirmButtonText: 'Aceptar',
+                cancelButtonText: 'Cancelar',
+                allowOutsideClick:false,
+                confirmButtonColor: '#4D8BA0',
+            });
+        }
+    },
+}
