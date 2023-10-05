37bf21854fe7c00549367e460b20b7d8fd63dfc2
Alex Velasquez <alexvelasquez94@gmail.com>
2023-10-05 17:13:23 -0300

    [ADD] installation of dependencies

--- src/main.js
+++ src/main.js
@@ -5,19 +5,24 @@ import { router } from './router'
 import { vuetify } from './plugins/vuetify'
 import { createPinia } from 'pinia'
 
-
 import Axios from './plugins/axios'
+
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
 import './assets/custom.css'
 import 'animate.css';
 
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+import GlobalMixin from './mixins/global'
 const app = createApp(App)
     .use(router)
     .use(vuetify)
     .use(pinia)
     .use(VueSweetalert2)
+    .mixin(GlobalMixin)
 app.config.globalProperties.$http = Axios
 app.mount('#app')
