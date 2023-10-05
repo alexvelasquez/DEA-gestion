ccb2a2c705b88f962f78de75f148979757ab0147
Alex Velasquez <alexvelasquez94@gmail.com>
2023-10-05 17:16:11 -0300

    [ADD] store user and rol

--- src/stores/app.js
+++ src/stores/app.js
@@ -1,8 +1,26 @@
 import { defineStore } from 'pinia'
 
+
 export const useAppStore = defineStore('app', {
     state: () => ({
-        rol: 'REPRESENTANTE',
+        user: null,
+        loadingApp:false,
         menuUser: []
     }),
+    getters: {
+        rol() {
+            const roles = {
+                "administrador_provincial": "Administrador Provincial",
+                "representante": "Representante",
+                "certificador": "CERTIFICADOR"
+            }
+            return this.user ? roles[this.user.rol] : null
+        }
+    },
+    persist: [
+        {
+            paths: ['user'],
+            storage: localStorage,
+        },
+    ]
 })
