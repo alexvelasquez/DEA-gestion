4c081335cbd4598144c695cd27135ef5209cda5e
Alex Velasquez <alexvelasquez94@gmail.com>
2023-10-05 17:15:20 -0300

    [ADD] interceptors for token validation

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,5 +1,40 @@
 import axios from 'axios'
 
-export default axios.create({
-    baseURL: 'https://jsonplaceholder.typicode.com',
+const http = axios.create({
+    baseURL: import.meta.env.VITE_API_ENDPOINT
 });
+
+// Guard para agregar token en los request
+http.interceptors.request.use(
+    config => {
+        let accessToken = localStorage.getItem('token') || null;
+        if (accessToken) {
+            config.headers = Object.assign({
+                Authorization: `Bearer ${accessToken}`
+            }, config.headers);
+        }
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+// Guard para controlar vencimiento de token
+http.interceptors.response.use(
+    response => {
+        return response;
+    },
+    error => {
+        const {
+            response: { status, statusText }
+        } = error;
+        if (status === 401 && statusText === 'Unauthorized') {
+            localStorage.clear();
+            window.location.href = '/login'
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default http
